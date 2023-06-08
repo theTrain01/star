9037830224fb6da8d84c00e7716d542a8816ed6b
theTrain01 <tima-murzabekov.com@mail.ru>
2023-06-08 21:27:24 +0300

    fix

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -7,20 +7,26 @@ import PeopleList from '../../components/PeoplePage/PeopleList';
 
 const PeoplePage = () => {
     const [people, setPeople] = React.useState(null)
+    const [errorApi, setErrorApi] = React.useState(false)
     const getResourse = async (url)  => {
-        const res = await getApiResourse(url)
+        if(url) {
+            const res = await getApiResourse(url)
         
-        const peopleList = res.results.map(({name, url}) => {
-            const id = getPeopleId(url)
-            const img = getPeopleImage(id)
-            return {
-                id: id,
-                name: name, 
-                img: img
-            }
-        })
+            const peopleList = res.results.map(({name, url}) => {
+                const id = getPeopleId(url)
+                const img = getPeopleImage(id)
+                return {
+                    id: id,
+                    name: name, 
+                    img: img
+                }
+            })
 
-        setPeople(peopleList)
+            setPeople(peopleList)
+            setErrorApi(false)
+        } else {
+            setErrorApi(true)
+        }
     }
     React.useEffect(() => {
         getResourse(API_PEOPLE)
@@ -28,7 +34,11 @@ const PeoplePage = () => {
 
     return (
         <>
-            {people && <PeopleList people = {people}/>}
+            {errorApi
+                ? <h2>ERROR!</h2>
+                : people && <PeopleList people = {people}/>
+            }
+            
         </>
     )
 }
